5019ebc338e2b1d3d0173c9b153ee231cafeaa60
PRZEMEK-ASUS\Przemek <przemek.nauka+Atlassian@gmail.com>
2017-01-11 00:45:27 +0100

    add type field

--- src/controllers/DaneController.java
+++ src/controllers/DaneController.java
@@ -7,30 +7,28 @@ import model.Dane;
 
 //@RestController
 public class DaneController {
-	
-	//@RequestMapping(path="/nuda")
+
+	// @RequestMapping(path="/nuda")
 	public List<Dane> getNuda() {
 		List<Dane> result = new ArrayList<>();
-		result.add(new Dane(1, "Pizza", "Najlepsza pizza", "2.jpg", 3.14156, 5.12459,5, "www.google.pl", "Zgierz, ul. £ódzka 4", "42 569 56 56"));
+		result.add(new Dane(1, "Pizza", "Najlepsza pizza", "2.jpg", 3.14156, 5.12459, 5, "www.google.pl",
+				"Zgierz, ul. £ódzka 4", "42 569 56 56", "nic"));
 		return result;
 	}
-	
-	
-	//@RequestMapping(path="/zmeczenie")
+
+	// @RequestMapping(path="/zmeczenie")
 	public List<Dane> getZmeczenie() {
 		List<Dane> result = new ArrayList<>();
 		return result;
 	}
-	
-	
-	//@RequestMapping(path="/glod")
+
+	// @RequestMapping(path="/glod")
 	public List<Dane> getGlod() {
 		List<Dane> result = new ArrayList<>();
 		return result;
 	}
-	
-	
-	//@RequestMapping(path="/zakupy")
+
+	// @RequestMapping(path="/zakupy")
 	public List<Dane> getZakupy() {
 		List<Dane> result = new ArrayList<>();
 		return result;
--- src/model/Dane.java
+++ src/model/Dane.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Dane {
-	
+
 	private int id;
 	private String name;
 	private String info;
@@ -12,12 +12,10 @@ public class Dane {
 	private String www;
 	private String adress;
 	private String phone;
-	//private String type;
-	
-	
-	
-	public Dane(int id, String name, String info, String marker_icon, double lat, double lng, int rating,
-			String www, String adress, String phone) {
+	private String type;
+
+	public Dane(int id, String name, String info, String marker_icon, double lat, double lng, int rating, String www,
+			String adress, String phone, String type) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -29,98 +27,95 @@ public class Dane {
 		this.www = www;
 		this.adress = adress;
 		this.phone = phone;
-		//this.type = type;
+		this.type = type;
 	}
-	
-	
-	
-
 
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 	public String getInfo() {
 		return info;
 	}
+
 	public void setInfo(String info) {
 		this.info = info;
 	}
-	
-	
+
 	public String getMarker_icon() {
 		return marker_icon;
 	}
+
 	public void setMarker_icon(String marker_icon) {
 		this.marker_icon = marker_icon;
 	}
-	
-	
+
 	public double getLat() {
 		return lat;
 	}
+
 	public void setLat(long lat) {
 		this.lat = lat;
 	}
-	
-	
+
 	public double getLng() {
 		return lng;
 	}
+
 	public void setLng(long lng) {
 		this.lng = lng;
 	}
-	
-	
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	
-	
+
 	public String getWww() {
 		return www;
 	}
+
 	public void setWww(String www) {
 		this.www = www;
 	}
-	
-	
+
 	public String getAdress() {
 		return adress;
 	}
+
 	public void setAdress(String adress) {
 		this.adress = adress;
 	}
-	
-	
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
-//	public String getType() {
-//		return type;
-//	}
-//	public void setType(String type) {
-//		this.type = type;
-//	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 }
